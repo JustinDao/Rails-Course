9b3337f0a27392002bb92616dff9539170793589
Justin Dao <tjd5at@virginia.edu>
2015-01-26 23:20:11 -0500

    Updates for course

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase) 
     if user && user.authenticate(params[:session][:password]) 
       session[:remember_token] = user.id
-      redirect_to user_path(user) # redirect_to user
+      redirect_to root_path
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
